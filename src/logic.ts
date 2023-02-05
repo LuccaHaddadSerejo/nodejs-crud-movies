c4ba4ca4b325d45db43d976db83c022be7b88e26
LuccaHaddadSerejo <hslucca@gmail.com>
2023-02-05 05:17:47 -0300

    feat: get all movies filters

--- src/logic.ts
+++ src/logic.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import format from "pg-format";
-import { QueryConfig } from "pg";
 import { client } from "./database";
 import { iMovie, movieResult, iPaginationMoviesRes } from "./interfaces";
 
@@ -23,57 +22,88 @@ const createMovie = async (req: Request, res: Response): Promise<Response> => {
 };
 
 const getAllMovies = async (req: Request, res: Response): Promise<Response> => {
-  const checkPerPageValue = (param: any) => {
-    if (param === undefined || param > 5 || param <= 0) {
-      return 5;
-    } else {
-      return param;
-    }
-  };
+  const perPage: any =
+    req.query.perPage === undefined ||
+    +req.query.perPage > 5 ||
+    +req.query.perPage <= 0
+      ? 5
+      : req.query.perPage;
 
-  const checkPageValue = (param: any) => {
-    if (param === undefined || param <= 0) {
-      return 0;
-    } else {
-      return param;
-    }
-  };
+  const page: any =
+    req.query.page === undefined || +req.query.page <= 0 ? 0 : req.query.page;
 
-  const perPage: any = checkPerPageValue(req.query.perPage);
-  const page: any = checkPageValue(req.query.page);
-  const offset = page * perPage;
+  const offset: number = page * perPage;
 
-  const queryString: string = `SELECT * FROM movies LIMIT $1 OFFSET $2`;
+  const sort: string | undefined = req.query.sort + "";
+  const order: string | undefined = req.query.order + "";
+  const sortOptions: string[] = ["price", "duration"];
+  const orderOptions: string[] = ["desc", "asc"];
+
   const queryCount: string = `SELECT COUNT(*) FROM movies`;
 
-  const queryConfig: QueryConfig = {
-    text: queryString,
-    values: [perPage, offset],
-  };
+  let queryString: string = "";
+  if (sort === undefined || sortOptions.includes(sort) === false) {
+    queryString = format(
+      `SELECT * FROM movies
+       LIMIT %s 
+       OFFSET %s;
+      `,
+      perPage,
+      offset
+    );
+  } else if (
+    sortOptions.includes(sort) &&
+    orderOptions.includes(order) === false
+  ) {
+    queryString = format(
+      `SELECT * FROM movies
+      ORDER BY %s ASC
+      LIMIT %s 
+      OFFSET %s;
+      `,
+      sort,
+      perPage,
+      offset
+    );
+  } else {
+    queryString = format(
+      `SELECT * FROM movies
+       ORDER BY %s %s
+       LIMIT %s 
+       OFFSET %s;
+      `,
+      sort,
+      order,
+      perPage,
+      offset
+    );
+  }
 
-  const queryResult: movieResult = await client.query(queryConfig);
+  const queryStringResult: movieResult = await client.query(queryString);
   const queryCountResult: any = await client.query(queryCount);
 
-  const previousPage: string | null =
-    +req.query.page! !== 0 ? +req.query.page! - 1 + "" : null;
-  const nextPage: string = +req.query.page! + 1 + "";
+  const previousPage: number | null = !Number.isNaN(+req.query.page!)
+    ? +req.query.page! - 1
+    : null;
+  const nextPage: number = !Number.isNaN(+req.query.page!)
+    ? +req.query.page! + 1
+    : 1;
 
-  const getUrl = req.get("host");
-  const itensRetrieved = queryResult.rows.length + offset;
-  const databaseItensCount = queryCountResult.rows[0].count;
-  console.log(itensRetrieved);
-  console.log(databaseItensCount);
+  const getUrl: string | undefined = req.get("host");
+  const itensRetrieved: number = queryStringResult.rows.length + offset;
+  const databaseItensCount: number = queryCountResult.rows[0].count;
+
   const treatedResult: iPaginationMoviesRes = {
     previousPage:
-      page === undefined || 1
+      page === undefined || page === 0
         ? null
-        : `${getUrl}/movies?page=${previousPage}&perPage=${page}`,
+        : `${getUrl}/movies?page=${previousPage}&perPage=${perPage}`,
     nextPage:
       databaseItensCount <= itensRetrieved
         ? null
         : `${getUrl}/movies?page=${nextPage}&perPage=${perPage}`,
-    count: queryResult.rowCount,
-    data: queryResult.rows,
+    count: queryStringResult.rowCount,
+    data: queryStringResult.rows,
   };
 
   return res.status(200).json(treatedResult);
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -2,6 +2,26 @@ import { NextFunction, Request, Response } from "express";
 import { client } from "./database";
 import { iMovie, iReqMovie, movieCreate } from "./interfaces";
 
+const checkIfMovieExist = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  const queryString: string = `SELECT * FROM movies`;
+  const queryResult = await client.query(queryString);
+  const movies = queryResult.rows;
+
+  const validationResult: movieCreate | undefined = movies.find(
+    (movie: iMovie) => movie.name === req.body.name
+  );
+
+  if (validationResult === undefined) {
+    return res.status(404).json({ message: "Movie dont exist" });
+  }
+
+  return next();
+};
+
 const checkUniqueMovie = async (
   req: Request,
   res: Response,
@@ -21,8 +41,9 @@ const checkUniqueMovie = async (
   };
 
   if (checkIfMovieAlreadyExists() !== undefined) {
-    return res.status(409).json({ message: "Movie already exists" });
+    return res.status(409).json({ message: "Movie already exist" });
   }
+
   return next();
 };
 
@@ -61,4 +82,4 @@ const checkDescription = async (
   return next();
 };
 
-export { checkUniqueMovie, checkDescription };
+export { checkUniqueMovie, checkDescription, checkIfMovieExist };
